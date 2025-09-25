cc3f06931dfde447091936cdc58f7a071fc26725
Mohammad Malekzadeh <malekmath021@gmail.com>
2025-09-25 23:48:52 +0330

    fix: fix some bug

---
+++ build/service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "loop-cache-v1";
+const urlsToCache = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});

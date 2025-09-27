6fc82b286a963c83dd09bf6a995264ac553a8a53
Mohammad Malekzadeh <malekmath021@gmail.com>
2025-09-27 22:00:23 +0330

    feat: add email link

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,4 @@
-import { FaInstagram, FaTelegramPlane, FaGithub, FaInfoCircle, FaPhoneAlt } from "react-icons/fa";
+import { FaInstagram, FaTelegramPlane, FaGithub, FaInfoCircle, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
@@ -7,7 +7,7 @@ export default function Footer() {
                       font-myfont right-farsi bg-jet">
       
       {/* Left Section */}
-      <div className="text-center md:text-left mb-4 md:mb-0 right-farsi items-center justify-center text-isabelline">
+      <div className="text-center md:text-left mb-4 md:mb-0 right-farsi items-center justify-center text-eggshell">
         <p className="font-bold text-lg items-center justify-center flex">لووپ © ۲۰۲۵</p>
         <div className="flex items-center gap-4 justify-center md:justify-start mt-2">
           <Link to="/contact-us" className="hover:text-mantis flex items-center gap-2 right-farsi">
@@ -20,14 +20,14 @@ export default function Footer() {
       </div>
 
       {/* Middle Section */}
-      <div className="text-center mb-4 md:mb-0 text-isabelline">
+      <div className="text-center mb-4 md:mb-0 text-eggshell">
         {/* <p className="text-sm">Powered by <span className="font-semibold">Malek</span></p> */}
         <p className="text-sm">توسعه داده شده توسط <a href="https://instagram.com/er._.math" className="font-semibold text-loop hover:text-mantis">مَلِک</a></p>
         <p className="text-sm">تمامی حقوق محفوظ است</p>
       </div>
 
       {/* Right Section */}
-      <div className="flex space-x-6 left-num text-isabelline">
+      <div className="flex space-x-6 left-num text-eggshell">
         <a
           href="https://instagram.com"
           target="_blank"
@@ -47,6 +47,16 @@ export default function Footer() {
           <FaTelegramPlane className="text-lg" />
           <span>Telegram</span>
           {/* <span> تلگرام </span> */}
+        </a>
+        <a
+          href="mailto:lloopforlife@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center space-x-2 hover:text-red-500 transition"
+        >
+          <FaEnvelope className="text-lg" />
+          <span>Email</span>
+          {/* <span> ایمیل </span> */}
         </a>
         <a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=653285&Code=RaAwR5KPF65j3auqo1ipoHjAb4FAEujk' className="w-16">
           <img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=653285&Code=RaAwR5KPF65j3auqo1ipoHjAb4FAEujk' alt='' style={{'cursor':'pointer'}} className="w-16" code='RaAwR5KPF65j3auqo1ipoHjAb4FAEujk' />
